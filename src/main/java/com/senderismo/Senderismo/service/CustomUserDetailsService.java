b39b300118a5d106410268f61e4c279daa02ccb4
santi <santi@DESKTOP-VJELQFI>
2024-09-30 13:02:01 -0500

    Carga Inicial

--- src/main/java/com/senderismo/Senderismo/service/CustomUserDetailsService.java
+++ src/main/java/com/senderismo/Senderismo/service/CustomUserDetailsService.java
@@ -2,13 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.senderismo.Senderismo.service.impl;
+package com.senderismo.Senderismo.service;
+
+import com.senderismo.Senderismo.exception.OurException;
+import com.senderismo.Senderismo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
 
-public class CustomUserDetailsService implements UserDetailService {
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
     
     @Override
-    public UserDetails 
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        return userRepository.findByEmail(username).orElseThrow(()-> new OurException("Username/Email not Found"));
+    }
     
 }
